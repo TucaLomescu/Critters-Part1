77ee7b45c915e4f260db76b3cb44242b1b59e4c9
amoghagnihotri <amoghagnihotri@utexas.edu>
2018-03-19 00:07:51 -0500

    updated changeLoc and walk and run methods

--- src/assignment4/Critter.java
+++ src/assignment4/Critter.java
@@ -52,13 +52,17 @@ public abstract class Critter {
 	private int y_coord;
 	
 	protected final void walk(int direction) {
+		energy -= Params.walk_energy_cost;
+		changeLoc(direction, true);
 	}
 	
 	protected final void run(int direction) {
-		
+		energy -= Params.run_energy_cost;
+		changeLoc(direction, false);
 	}
 	
 	protected final void reproduce(Critter offspring, int direction) {
+
 	}
 
 	public abstract void doTimeStep();
@@ -122,58 +126,6 @@ public abstract class Critter {
 		}
 		System.out.println();		
 	}
-	
-	/* the TestCritter class allows some critters to "cheat". If you want to 
-	 * create tests of your Critter model, you can create subclasses of this class
-	 * and then use the setter functions contained here. 
-	 * 
-	 * NOTE: you must make sure that the setter functions work with your implementation
-	 * of Critter. That means, if you're recording the positions of your critters
-	 * using some sort of external grid or some other data structure in addition
-	 * to the x_coord and y_coord functions, then you MUST update these setter functions
-	 * so that they correctly update your grid/data structure.
-	 */
-	static abstract class TestCritter extends Critter {
-		protected void setEnergy(int new_energy_value) {
-			super.energy = new_energy_value;
-		}
-		
-		protected void setX_coord(int new_x_coord) {
-			super.x_coord = new_x_coord;
-		}
-		
-		protected void setY_coord(int new_y_coord) {
-			super.y_coord = new_y_coord;
-		}
-		
-		protected int getX_coord() {
-			return super.x_coord;
-		}
-		
-		protected int getY_coord() {
-			return super.y_coord;
-		}
-		
-
-		/*
-		 * This method getPopulation has to be modified by you if you are not using the population
-		 * ArrayList that has been provided in the starter code.  In any case, it has to be
-		 * implemented for grading tests to work.
-		 */
-		protected static List<Critter> getPopulation() {
-			return population;
-		}
-		
-		/*
-		 * This method getBabies has to be modified by you if you are not using the babies
-		 * ArrayList that has been provided in the starter code.  In any case, it has to be
-		 * implemented for grading tests to work.  Babies should be added to the general population 
-		 * at either the beginning OR the end of every timestep.
-		 */
-		protected static List<Critter> getBabies() {
-			return babies;
-		}
-	}
 
 	/**
 	 * Clear the world of all critters, dead and alive
@@ -181,9 +133,15 @@ public abstract class Critter {
 	public static void clearWorld() {
 		// Complete this method.
 	}
-	
+
+	/**
+	 * Simulates one time step for every Critter in the population
+	 */
 	public static void worldTimeStep() {
-		// Complete this method.
+		for(Critter c: population){
+			c.doTimeStep();
+		}
+		//TODO check for resolving conflicts
 	}
 
     /**
@@ -203,5 +161,120 @@ public abstract class Critter {
         System.out.print("+");
         for(int i = 0; i < Params.world_width; i++) System.out.print("-");
         System.out.println("+");
+	}
+
+	/**
+	 * changes the x and y coordinates to update critter location
+	 * @param direction specifies movement direction
+	 * @param isWalk true if walk, false if run
+	 */
+	public void changeLoc(int direction, boolean isWalk){
+		int changeX, changeY;
+
+		switch(direction){
+			case 0:
+				changeX = 0;
+				changeY = -1;
+				break;
+			case 1:
+				changeX = 1;
+				changeY = -1;
+				break;
+			case 2:
+				changeX = 1;
+				changeY = 0;
+				break;
+			case 3:
+				changeX = 1;
+				changeY = 1;
+				break;
+			case 4:
+				changeX = 0;
+				changeY = 1;
+				break;
+			case 5:
+				changeX = -1;
+				changeY = 1;
+				break;
+			case 6:
+				changeX = -1;
+				changeY = 0;
+				break;
+			default:
+				changeX = -1;
+				changeY = -1;
+				break;
+		}
+
+		if(!isWalk){
+			changeX *= 2;
+			changeY *= 2;
+		}
+
+		if(x_coord + changeX < 0){
+			x_coord = Params.world_width + (x_coord + changeX);
+		} else{
+			x_coord = (x_coord + changeX) % (Params.world_width-1);
+		}
+
+		if(y_coord + changeY < 0){
+			y_coord = Params.world_height + (y_coord + changeY);
+		} else{
+			y_coord = (y_coord + changeY) % (Params.world_height-1);
+		}
+
+	}
+
+
+	/* the TestCritter class allows some critters to "cheat". If you want to
+	 * create tests of your Critter model, you can create subclasses of this class
+	 * and then use the setter functions contained here.
+	 *
+	 * NOTE: you must make sure that the setter functions work with your implementation
+	 * of Critter. That means, if you're recording the positions of your critters
+	 * using some sort of external grid or some other data structure in addition
+	 * to the x_coord and y_coord functions, then you MUST update these setter functions
+	 * so that they correctly update your grid/data structure.
+	 */
+	static abstract class TestCritter extends Critter {
+		protected void setEnergy(int new_energy_value) {
+			super.energy = new_energy_value;
+		}
+
+		protected void setX_coord(int new_x_coord) {
+			super.x_coord = new_x_coord;
+		}
+
+		protected void setY_coord(int new_y_coord) {
+			super.y_coord = new_y_coord;
+		}
+
+		protected int getX_coord() {
+			return super.x_coord;
+		}
+
+		protected int getY_coord() {
+			return super.y_coord;
+		}
+
+
+		/*
+		 * This method getPopulation has to be modified by you if you are not using the population
+		 * ArrayList that has been provided in the starter code.  In any case, it has to be
+		 * implemented for grading tests to work.
+		 */
+		protected static List<Critter> getPopulation() {
+			return population;
+		}
+
+		/*
+		 * This method getBabies has to be modified by you if you are not using the babies
+		 * ArrayList that has been provided in the starter code.  In any case, it has to be
+		 * implemented for grading tests to work.  Babies should be added to the general population
+		 * at either the beginning OR the end of every timestep.
+		 */
+		protected static List<Critter> getBabies() {
+			return babies;
+		}
 	}
 }
